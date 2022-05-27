4797b0f755106ae64fa0dfc2bef897922cf6d698
Savaksp <86986848+Savaksp@users.noreply.github.com>
2022-05-27 21:04:39 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,10 @@
 local Config = {
     WindowName = "Chertila.hub",
-	Color = Color3.fromRGB(255,128,64),
-	Keybind = Enum.KeyCode.RightBracket,
+    Color = Color3.fromRGB(255,128,64),
+    Keybind = Enum.KeyCode.RightBracket,
+
+    Players = {},
+    Player_SEL = nil,
 
     JF_AF = false,
     MS_AF = false,
@@ -14,11 +17,43 @@ local Config = {
     Chams_color = Color3.new(255,0,0),
     Chams_transp = 0
 }
-
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/BracketV3.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Savaksp/Chertila.hub/main/modified_lib.lua"))()
 local win = lib:CreateWindow(Config, game:GetService("CoreGui"))
 
+-- Update Players List
+local function upl()
+    Config['Players'] = {}
+    for i,v in pairs(game:GetService("Players"):GetChildren()) do
+        local plrs = Config['Players']
+        table.insert(plrs, v.Name)
+    end
+end
+
+-- Player tab
+local Player_TAB = win:CreateTab("Player")
+-- Teleport
+local Teleport_DRD = nil
+local Teleport_SEC = Player_TAB:CreateSection("Teleport")
+local Players_DRD = Teleport_SEC:CreateDropdown('Players', Config['Players'], function(plr)
+    Config['Player_SEL'] = plr
+end)
+local Teleport_BTN = Teleport_SEC:CreateButton('Teleport', function()
+    local player = game:GetService('Players')[Config['Player_SEL']]
+    if Config['Player_SEL'] ~= nil then
+        game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = player.Character.CFrame
+    end
+end)
+local UpdatePlayers_BTN = Teleport_SEC:CreateButton('Update players', function()
+    upl()
+    Players_DRD:ClearOptions()
+    Players_DRD:SetOptionsList(Config['Players'], function(plr)
+        Config['Player_SEL'] = plr
+    end)
+end)
+
+-- Visuals tab
 local Visuals_TAB = win:CreateTab("Visuals")
+-- Chams
 local Chams_SEC = Visuals_TAB:CreateSection("Chams")
 local Chams_TOG = Chams_SEC:CreateToggle("Toggle chams", nil, function()
     Config["Chams_bool"] = not Config["Chams_bool"]
@@ -51,7 +86,7 @@ local Chams_TOG = Chams_SEC:CreateToggle("Toggle chams", nil, function()
     end
 end)
 local Chams_SLD = Chams_SEC:CreateSlider("Chams transparency", 0,1,nil,false, function(value)
-	if Config["Chams_bool"] == true then
+    if Config["Chams_bool"] == true then
         for i,plr in pairs(game:GetService("Players"):GetChildren()) do
             if plr ~= game.Players.LocalPlayer then
                 for i,part in pairs(plr.Character:GetChildren()) do
@@ -67,7 +102,7 @@ local Chams_SLD = Chams_SEC:CreateSlider("Chams transparency", 0,1,nil,false, fu
     end
 end)
 local Chams_COL = Chams_SEC:CreateColorpicker("Chams color", function(value)
-	if Config["Chams_bool"] == true then
+    if Config["Chams_bool"] == true then
         for i,plr in pairs(game:GetService("Players"):GetChildren()) do
             if plr ~= game.Players.LocalPlayer then
                 for i,part in pairs(plr.Character:GetChildren()) do
@@ -81,4 +116,12 @@ local Chams_COL = Chams_SEC:CreateColorpicker("Chams color", function(value)
     else    
         Config["Chams_color"] = value
     end
+end)
+-- Time
+local Time_SEC = Visuals_TAB:CreateSection("Time")
+local Day_BTN = Time_SEC:CreateButton("Day", function()
+    game:GetService('Lighting').TimeOfDay = '12:00:00'
+end)
+local Night_BTN = Time_SEC:CreateButton("Night", function()
+    game:GetService('Lighting').TimeOfDay = '00:00:00'
 end)
